6b5dbb36da3b33013b6eb55527cb59d0f6e3659a
Charles Moncada <charles.moncada@emoshiapps.com>
2016-10-25 16:03:57 -0500

    Hecha la ventana de newScoop y la funcionalidad, sin meterle aun Azure

---
+++ Scoops/Scoops/Scoops-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Scoops-Bridging-Header.h
+//  Scoops
+//
+//  Created by Charles Moncada on 23/10/16.
+//  Copyright © 2016 Charles Moncada. All rights reserved.
+//
+
+#ifndef Scoops_Bridging_Header_h
+#define Scoops_Bridging_Header_h
+
+
+#endif /* Scoops_Bridging_Header_h */
